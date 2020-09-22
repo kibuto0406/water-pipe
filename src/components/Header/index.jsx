03c2db12f3cf6d204c580f8e9304c5a5a14f445a
Kibuto <trongnhan.nguyen@citynow.vn>
2020-09-22 19:45:03 +0700

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <div className="header-wrapper">Header</div>;
+
+export default Header;
---
+++ src/pages/Detail/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Detail = () => <div className="detail-wrapper">Detail</div>;
+
+export default Detail;
