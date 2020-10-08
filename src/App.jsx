7dc48b431203179bd8050400eae9b2761435ea12
Kibuto <trongnhan.nguyen@citynow.vn>
2020-10-08 21:07:35 +0700

    import antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
-const App = () => <div>ReactJS</div>;
+const App = () => (
+	<div>
+		ReactJS{" "}
+		<Button type="primary" icon={<SearchOutlined />}>
+			Search
+		</Button>
+	</div>
+);
 
 export default App;
